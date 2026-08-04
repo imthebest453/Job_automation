6e750abd1ac44cc5f2868d390ba203934f9770a3
imthebest453 <your-github-email@example.com>
2026-08-04 20:19:37 +0100

    Connect dashboard to database

--- dashboard/server.py
+++ dashboard/server.py
@@ -3,16 +3,26 @@ from flask import Flask, render_template
 from database.database import SessionLocal
 from database.job_rep import JobRepository
 
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
+def dashboard():
 
     db = SessionLocal()
 
-    jobs = JobRepository.get_all_jobs(db)
+    try:
+        jobs = JobRepository.get_all(db)
 
-    return render_template("index.html", jobs=jobs)
+        return render_template(
+            "dashboard.html",
+            jobs=jobs
+        )
+
+    finally:
+        db.close()
+
 
 
 if __name__ == "__main__":
