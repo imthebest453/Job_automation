751a44986bee66beaaf4103286e1650cbb5c77f9
imthebest453 <mafongaming@gmail.com>
2026-08-04 21:44:10 +0100

    Delete Job_Automation directory

--- Job_Automation/scrapers/base_scraper.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseScraper(ABC):
-
-    @abstractmethod
-    def search(self, keyword: str):
-        """Return a list of job dictionaries."""
-        pass
